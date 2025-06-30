b8b79c30d89dbbaec66a0af74def684c97cc45e0
Shrimptempura <ongtzxowns98@gmail.com>
2025-06-30 16:50:24 +0900

    feat: 이미지 저장 관련 Dto

---
+++ src/main/java/com/tech/prjm09/dto/ReBrdimgDto.java
@@ -0,0 +1,18 @@
+package com.tech.prjm09.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+public class ReBrdimgDto {
+
+	private int rebno;
+	private int bid;
+	private String reborgfile;
+	private String rebchgfile;
+}
