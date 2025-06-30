b4727308280b749ba94700cde4aeb93950aed418
Shrimptempura <ongtzxowns98@gmail.com>
2025-06-30 16:44:59 +0900

    project setup main init

---
+++ src/main/java/com/tech/prjm09/dao/IDao.java
@@ -0,0 +1,29 @@
+package com.tech.prjm09.dao;
+
+import java.util.ArrayList;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.tech.prjm09.dto.BDto;
+
+@Mapper
+public interface IDao {
+//	ArrayList<BDto> list();
+//	ArrayList<BDto> list(int start, int end);
+	ArrayList<BDto> list(int start, int end, String sk, String selNum);
+	void write(String bname, String btitle, String bcontent);
+	BDto contentView(String sbid);
+	BDto modifyView(String sbid);
+	void modify(String bid, String bname, String btitle, String bcontent);
+	BDto reply_view(String sbid);
+	void reply(String bid, String bname, String btitle, String bcontent, 
+			String bgroup, String bstep, String bindent);
+	int replyShape(String strgroup, String strstep);
+	void delete(String bid);
+	void upHit(String sbid);
+//	int selectBoardCount();
+	int selectBoardCount(String sk, String selNum);
+
+
+
+}
